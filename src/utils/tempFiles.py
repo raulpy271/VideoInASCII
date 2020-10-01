40231b1c3c5a2aa1d3743b91cd6e1c0644b5978c
Raul <raulrufino0@gmail.com>
2020-10-01 13:34:10 -0300

    fixing usage of isfile instead of isdir

--- src/utils/tempFiles.py
+++ src/utils/tempFiles.py
@@ -1,18 +1,18 @@
 from shutil import rmtree
 from os import mkdir
-from os.path import isfile
+from os.path import isdir
 
 from .constants import (
     tempDir)
 
 
 def createTempDir():
-    if not isfile(tempDir):
+    if not isdir(tempDir):
         mkdir(tempDir)
 
 
 def removeTempDir():
-    if isfile(tempDir):
+    if isdir(tempDir):
         rmtree(tempDir)
 
 
