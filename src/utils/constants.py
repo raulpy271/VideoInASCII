13318c4e7e8b9eb8aa94129a9f03ad0e784c421b
Raul <raulrufino0@gmail.com>
2020-10-08 21:58:31 -0300

    removing magic number

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,9 +1,10 @@
 tempDir = '.tmp/'
 defaultPath = 'ballet.mp4'
-videoResizedPath = tempDir + "videoResized.mp4"
-videoResizedFrameRate = 5
-ascii_chars = ('.', '^', '&', 'X', '#')
-ffmpegGlobalArguments = ['-y', '-loglevel', '16']
+videoResizedPath = tempDir + "videoResized.raw"
+videoResizedFrameRate = 10
+showAllErrorsFlag = '16'
+ffmpegGlobalArguments = ['-y', '-loglevel', showAllErrorsFlag]
+ascii_chars = ('X', '#', '&', '^', '.', ' ')
 
 
 class VideoPath():
